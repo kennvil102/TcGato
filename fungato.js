8c82ff52f1fc221f45fb92ce2496a9a3b4961895
kennvil02 <kennvilchez@gmail.com>
2024-05-20 22:53:45 -0600

    update

---
+++ fungato.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll(".cuadrado");
+let turno = true;
+let enjuego= true;
+const reiniciar = document.querySelector("#reiniciar");
+let ganadasx=0;
+let ganadas0=0;
+let empates=0;
+
+ 
