d0d4584a2edb586fc40772d3041f2a761ab620f0
kennvil102 <kennvilchez@gmail.com>
2024-05-24 13:13:26 -0600

    creacion23

--- fungato.js
+++ fungato.js
@@ -1,11 +1,15 @@
+
+// Llamamos a los elementos por clase
 const buttons = document.querySelectorAll(".cuadrado");
+// Hacemos las variables para el turno y por si el juego sigue activo
 let turno = true;
 let in_game = true;
 const reiniciar = document.querySelector("#reiniciar");
+// Hacemos las variables que mostraremos atraves de un span
 let ganadasx = 0;
 let ganadas0 = 0;
 let empates = 0;
-/* actualiza las variables ganx gan0 y empates llamandolas y aplicandole el textcontent*/
+// Llamamos a los elementos por su id y le aplicamos textContent esta función sirve para actualizar
 function update() {
     document.querySelector("#ganadasX").textContent = "Juegos ganados de X: " + ganadasx;
     document.querySelector("#ganadasY").textContent = "Juegos ganados de O: " + ganadas0;
@@ -21,28 +25,28 @@ function jugarTurnoDeMaquina() {
         }
     });
 
-    // Elegir una celda vacía aleatoriamente
+    // Elegir una celda vacia aleatoriamente
     const indiceAleatorio = Math.floor(Math.random() * celdasVacias.length);
     const celdaAleatoria = celdasVacias[indiceAleatorio];
 
-    // Jugar en la celda aleatoria
+    // Juega en una celda aleatoria
     buttons[celdaAleatoria].textContent = "O";
 
-    // cambia el turno y ve si hay un ganador
+    // Cambia el turno y ve si hay un ganador
     turno = !turno;
     if (ganador() !== -1) {
-        // muesta el resultado
+        //  fun muesta el resultado
         mostrarResultado();
     }
 }
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", (e) => {
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
         if (turno && e.target.textContent === "" && in_game) {
             e.target.textContent = "X";
             turno = !turno;
             if (ganador() === 1) {
-                document.querySelector("#winner").textContent = alert("El Ganador fue X");
+                document.querySelector("#winner").textContent = "El Ganador fue X";
                 ganadasx++;
                 update();
             } else if (ganador() === -1) {
@@ -50,24 +54,24 @@ for (let i = 0; i < buttons.length; i++) {
                 empates++;
                 update();
             } else {
-                jugarTurnoDeMaquina(); // La máquina juega su turno
+                jugarTurnoDeMaquina(); // La maquinita juega su turno
             }
         }
     });
-}
+});
 
 reiniciar.addEventListener("click", (e) => {
     in_game = true;
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].textContent = "";
-    }
+    buttons.forEach((button) => {
+        button.textContent = "";
+    });
     document.querySelector("#winner").textContent = "";
     contador = 0;
 });
-
+ /* Hacemos un array con las lista de combinaciones ganadoras*/
 const list = [
     [0, 1, 2],
-    [3, 4, 5],
+    [3, 4, 5], 
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
@@ -75,9 +79,9 @@ const list = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
+/* Hacemos el contador movimientos */
 let contador = 0;
-
+/* Función ganadora, luego iteramos verifica las casilas que estan en X */
 function ganador() {
     for (let i = 0; i < list.length; i++) {
         if (buttons[list[i][0]].textContent === "X" && buttons[list[i][1]].textContent === "X" &&
@@ -86,16 +90,17 @@ function ganador() {
             return 1;
         } else if (buttons[list[i][0]].textContent === "O" && buttons[list[i][1]].textContent === "O" &&
             buttons[list[i][2]].textContent === "O") {
-            in_game = false;
+            in_game = false; // Verifica ganador y el juego deja de estar activo
             return 0;
         }
     }
-    if (contador === 9) {
+    if (contador === 9) { // Si se realizan los 9 movimientos quiere decir que hay empate
         in_game = false;
         return -1;
     }
 }
-
+ /* Función mostrar el resultado en los span del HTML gana X lo actualiza, lo mismo con O Si
+ no queda empate  */
 function mostrarResultado() {
     if (ganador() === 1) {
         document.querySelector("#winner").textContent = "El Ganador fue X";
